9f6993111ac2a3ab17a4d77d560df2546a2cc9c7
prajwalraob <33778402+prajwalraob@users.noreply.github.com>
2021-08-12 21:01:07 +0530

    Notes icon with text

---
+++ EncryptNote/Views/DateToInteractiveStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EncryptNote.Views
+{
+    class DateToInteractiveStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime timeVal = (DateTime)value;
+            TimeSpan timeSpan = DateTime.Now.Subtract(timeVal);
+            if (timeSpan.Days < 1) return "Updated less than a day ago";
+            else return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
